c98ca2d5f292e0a071c709c4bf5ca358fb65d791
Roger Espinosa <roger@umich.edu>
2019-07-08 10:55:21 -0400

    import of imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import * as core from '@umich-lib/core'
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// // import * as core from '@umich-lib/core'
 
-var e = React.createElement;
-// ReactDOM.render(e(core.Text, { size: "2XL" }, "HELLO WORLD"), document.getElementById('root'));
+// var e = React.createElement;
 
 // const factory = function(component, kw, text) {
 //   return e(core[component], kw, text);
@@ -14,30 +13,55 @@ var e = React.createElement;
 //   ReactDOM.render(fn, document.getElementById(id));
 // }
 
-function load(id, module, kw, text) {
-  switch(module) {
-    case 'UniversalHeader':
-      import(/* webpackChunkName: "universal-header" */ '@umich-lib/universal-header').then(module => {
-        const component = module.default;
-        ReactDOM.render(e(component, kw, text), document.getElementById(id));
-      });
-      break;
-    case 'Alert':
-      import(/* webpackChunkName: "alert" */ '@umich-lib/alert').then(module => {
-        const component = module.default;
-        ReactDOM.render(e(component, kw, text), document.getElementById(id));
-      });
-      break;
-    case 'Chat':
-      import(/* webpackChunkName: "chat" */ '@umich-lib/chat').then(module => {
-        const component = module.default;
-        ReactDOM.render(e(component, kw, text), document.getElementById(id));
-      });
-      break;
-    default:
-      console.log("Sorry.");
-  }
+function load(id, component, kw, text) {
+  import(/* webpackChunkName: "react" */ 'react').then(module => {
+    const React = module.default;
+    import(/* webpackChunkName: "react-dom" */ 'react-dom').then(module => {
+      const ReactDOM = module.default;
+      import(/* webpackChunkName: "umich-lib" */ '@umich-lib/core').then(module => {
+        const umichLib = module;
+        console.log("AHOY", umichLib);
+        var widget;
+        if ( component == 'UniversalHeader' ) {
+          widget = React.createElement(
+            React.Fragment,
+            {},
+            React.createElement(umichLib['GlobalStyleSheet']),
+            React.createElement(umichLib[component])
+          );
+        } else {
+          widget = React.createElement(umichLib[component], kw, text);
+        }
+        ReactDOM.render(widget, document.getElementById(id));
+      })
+    })
+  })
 }
+
+// function load(id, module, kw, text) {
+//   switch(module) {
+//     case 'UniversalHeader':
+//       import(/* webpackChunkName: "universal-header" */ '@umich-lib/universal-header').then(module => {
+//         const component = module.default;
+//         ReactDOM.render(e(component, kw, text), document.getElementById(id));
+//       });
+//       break;
+//     case 'Alert':
+//       import(/* webpackChunkName: "alert" */ '@umich-lib/alert').then(module => {
+//         const component = module.default;
+//         ReactDOM.render(e(component, kw, text), document.getElementById(id));
+//       });
+//       break;
+//     case 'Chat':
+//       import(/* webpackChunkName: "chat" */ '@umich-lib/chat').then(module => {
+//         const component = module.default;
+//         ReactDOM.render(e(component, kw, text), document.getElementById(id));
+//       });
+//       break;
+//     default:
+//       console.log("Sorry.");
+//   }
+// }
 
 export {
   // factory,
