3ffbe05b7cd9ba050b29cd9d7e1acabaef4f0500
Roger Espinosa <roger@umich.edu>
2019-07-09 13:55:16 -0400

    documenting

--- README.md
+++ README.md
@@ -4,21 +4,23 @@
 
 Build a complete UMD distribution for umich-lib-ui by building a "library" with webpack.
 
-This experiment exposes a `DS` global which exports two methods:
+This experiment: set up `index.js` switches the chunking to chunk on `React`, `ReactDOM`, `@umich-lib` boundaries. The chunking splits dist/main.js into
+
+* `dist/main.js`
+* `dist/vendors~react-dom~umich-lib.bundle.js`
+* `dist/vendors~react~react-dom~umich-lib.bundle.js`
+* `dist/vendors~umich-lib.bundle.js`
+
+So fewer chunks.
+
+This experiment exposes a DS global which exports a method:
 
 ```
-var component = DS.factory(componentName, props, text)
-
-var textComponent = DS.factory('Text', { size: '2XL', 'Hello world'})
+DS.load('universal-header', 'UniversalHeader');
+DS.load('root', 'Alert', {}, 'AHOY WORLD');
 ```
 
-and
-
-```
-DS.render(component, 'id');
-
-DS.render(textComponent, 'root');
-```
+Note in `index.js` that asking for the `universal-header` will pull in the global stylesheet component.
 
 ## Running
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// // import * as core from '@umich-lib/core'
-
-// var e = React.createElement;
-
-// const factory = function(component, kw, text) {
-//   return e(core[component], kw, text);
-// }
-
-// const render = function(fn, id) {
-//   ReactDOM.render(fn, document.getElementById(id));
-// }
-
 function load(id, component, kw, text) {
   import(/* webpackChunkName: "react" */ 'react').then(module => {
     const React = module.default;
@@ -20,7 +6,6 @@ function load(id, component, kw, text) {
       const ReactDOM = module.default;
       import(/* webpackChunkName: "umich-lib" */ '@umich-lib/core').then(module => {
         const umichLib = module;
-        console.log("AHOY", umichLib);
         var widget;
         if ( component == 'UniversalHeader' ) {
           widget = React.createElement(
@@ -37,31 +22,6 @@ function load(id, component, kw, text) {
     })
   })
 }
-
-// function load(id, module, kw, text) {
-//   switch(module) {
-//     case 'UniversalHeader':
-//       import(/* webpackChunkName: "universal-header" */ '@umich-lib/universal-header').then(module => {
-//         const component = module.default;
-//         ReactDOM.render(e(component, kw, text), document.getElementById(id));
-//       });
-//       break;
-//     case 'Alert':
-//       import(/* webpackChunkName: "alert" */ '@umich-lib/alert').then(module => {
-//         const component = module.default;
-//         ReactDOM.render(e(component, kw, text), document.getElementById(id));
-//       });
-//       break;
-//     case 'Chat':
-//       import(/* webpackChunkName: "chat" */ '@umich-lib/chat').then(module => {
-//         const component = module.default;
-//         ReactDOM.render(e(component, kw, text), document.getElementById(id));
-//       });
-//       break;
-//     default:
-//       console.log("Sorry.");
-//   }
-// }
 
 export {
   // factory,
